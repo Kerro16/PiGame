32a9f860fe510ad60de5ab34781c600d64c3b847
TheR20 <r20_banana@hotmail.com>
2023-12-02 15:34:55 -0600

    Cambio de comentarios, y se agrega configuracion para Metal Slug

--- main.py
+++ main.py
@@ -2,73 +2,156 @@ import time
 import pyautogui
 
 time.sleep(5)
-file = open('C:/Users/fas_c/Desktop/pi-billion.txt')
+#Cerdo
+#file = open('C:/Users/fas_c/Desktop/pi-billion.txt')
+#R20
+file = open('C:/Users/R20/Desktop/pi-billion.txt')
 content = file.read()
 file.close()
-def switch(numero):
 
+
+#Metal Slug /arriba, abajo, izquierda, derecha, a , s , d  se agrega un enter cada que el index pasa por 10 iteraciones/
+
+
+#def iteraciones(index):
+ #   if index + 10
+def switch(numero, index):
     if numero == "0":
-        print("Arriba")
+        print("Arriba + Disparar")
         pyautogui.keyDown('up')
+        pyautogui.keyDown('a')
         time.sleep(1)
         pyautogui.keyUp('up')
-        return numero
+        pyautogui.keyUp('a')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "1":
         print("Abajo")
         pyautogui.keyDown('down')
         time.sleep(1)
         pyautogui.keyUp('down')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "2":
         print("Izquierda")
         pyautogui.keyDown('left')
         time.sleep(1)
         pyautogui.keyUp('left')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "3":
         print("Derecha")
         pyautogui.keyDown('right')
         time.sleep(1)
         pyautogui.keyUp('right')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "4":
+        print("Disparar")
+        pyautogui.keyDown('a')
+        time.sleep(1)
+        pyautogui.keyUp('a')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "5":
+        print("Granada")
+        pyautogui.keyDown('s')
+        time.sleep(1)
+        pyautogui.keyUp('s')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "6":
+        print("Derecha + Disparar")
+        pyautogui.keyDown('a')
+        pyautogui.keyDown('right')
+        time.sleep(1)
+        pyautogui.keyUp('right')
+        pyautogui.keyUp('a')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "7":
+        print("Arriba + Disparo")
+        pyautogui.keyDown('up')
+        pyautogui.keyDown('a')
+        time.sleep(1)
+        pyautogui.keyUp('up')
+        pyautogui.keyUp('a')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "8":
+        print("Salto + Derecha")
+        pyautogui.keyDown('d')
+        pyautogui.keyDown('right')
+        time.sleep(1)
+        pyautogui.keyUp('right')
+        pyautogui.keyUp('d')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "9":
+        print("Salto + Izquierda")
+        pyautogui.keyDown('d')
+        pyautogui.keyDown('left')
+        time.sleep(1)
+        pyautogui.keyUp('left')
+        pyautogui.keyUp('d')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+
+
+#Vampire Survivors /arriba, abajo, izquierda, derecha y enter/
+"""def switch(numero, index):
+    if numero == "0":
+        print("Arriba")
+        pyautogui.keyDown('up')
+        time.sleep(1)
+        pyautogui.keyUp('up')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "1":
+        print("Abajo")
+        pyautogui.keyDown('down')
+        time.sleep(1)
+        pyautogui.keyUp('down')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "2":
+        print("Izquierda")
+        pyautogui.keyDown('left')
+        time.sleep(1)
+        pyautogui.keyUp('left')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+    elif numero == "3":
+        print("Derecha")
+        pyautogui.keyDown('right')
+        time.sleep(1)
+        pyautogui.keyUp('right')
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "4":
         print("Enter")
         pyautogui.keyDown('enter')
         time.sleep(1)
         pyautogui.keyUp('enter')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "5":
         print("Izquierda")
         pyautogui.keyDown('left')
         time.sleep(1)
         pyautogui.keyUp('left')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "6":
         print("Derecha")
         pyautogui.keyDown('right')
         time.sleep(1)
         pyautogui.keyUp('right')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "7":
         print("Arriba")
         pyautogui.keyDown('up')
         time.sleep(1)
         pyautogui.keyDown('up')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "8":
         print("Abajo")
         pyautogui.keyDown('down')
         time.sleep(1)
         pyautogui.keyUp('down')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
     elif numero == "9":
         print("Enter")
         pyautogui.keyDown('enter')
         time.sleep(1)
         pyautogui.keyUp('enter')
-        return numero
+        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)"""
 
 
-for numero in range(len(content)):
-    numeroPI = content[numero]
-    print(switch(numeroPI))
+for index in range(len(content)):
+    numeroPI = content[index]
+    print(switch(numeroPI, index))
