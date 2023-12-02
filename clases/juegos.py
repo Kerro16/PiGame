de68efea7bca8b1432e807f2d3872a7d6522c000
TheR20 <r20_banana@hotmail.com>
2023-12-02 17:34:52 -0600

    Refactorizacion de codigo en jueguitos

--- clases/juegos.py
+++ clases/juegos.py
@@ -2,6 +2,29 @@ import time
 import pyautogui
 pyautogui.FAILSAFE = False #Elimina un crash por mover el mouse o el teclado a cierta parte
 
+
+def presionarBotonDoble(letraUno, letraDos, index,numero, movimiento):
+    print("Enter automatico por el indice en {}".format(index))
+    pyautogui.keyDown(letraUno)
+    pyautogui.keyDown(letraDos)
+    time.sleep(1)
+    pyautogui.keyUp(letraUno)
+    pyautogui.keyUp(letraDos)
+    return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+def presionarBotonSencillo(letra, index,numero, movimiento):
+    print("Enter automatico por el indice en {}".format(index))
+    pyautogui.keyDown(letra)
+    time.sleep(1)
+    pyautogui.keyUp(letra)
+    return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+def enterautomatico(index):
+    print("Enter automatico por el indice en {}".format(index))
+    pyautogui.keyDown('enter')
+    time.sleep(1)
+    pyautogui.keyUp('enter')
+
+
+
 #Vampire Survivors /arriba, abajo, izquierda, derecha y enter/
 def vampire(numero, index):
     if numero == "0":
@@ -69,77 +92,24 @@ def vampire(numero, index):
 # Metal Slug /arriba, abajo, izquierda, derecha, a , s , d  se agrega un enter cada que el index pasa por 9 iteraciones/
 def metalSlug(numero, index):
     if index%9 == 0:
-        print("Enter automatico por el indice en {}".format(index))
-        pyautogui.keyDown('enter')
-        time.sleep(1)
-        pyautogui.keyUp('enter')
+        enterautomatico(index)
     if numero == "0":
-        print("Arriba + Disparar")
-        pyautogui.keyDown('up')
-        pyautogui.keyDown('a')
-        time.sleep(1)
-        pyautogui.keyUp('up')
-        pyautogui.keyUp('a')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return  presionarBotonDoble('up', 'a', index,numero,'Arriba + Disparo')
     elif numero == "1":
-        print("Abajo")
-        pyautogui.keyDown('down')
-        time.sleep(1)
-        pyautogui.keyUp('down')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('down',index,numero,'Abajo')
     elif numero == "2":
-        print("Izquierda")
-        pyautogui.keyDown('left')
-        time.sleep(1)
-        pyautogui.keyUp('left')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('left',index,numero,'Izquierda')
     elif numero == "3":
-        print("Derecha")
-        pyautogui.keyDown('right')
-        time.sleep(1)
-        pyautogui.keyUp('right')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('right',index,numero,'Derecha')
     elif numero == "4":
-        print("Disparar")
-        pyautogui.keyDown('a')
-        time.sleep(1)
-        pyautogui.keyUp('a')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('a',index,numero,'Disparar')
     elif numero == "5":
-        print("Granada")
-        pyautogui.keyDown('s')
-        time.sleep(1)
-        pyautogui.keyUp('s')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('s',index,numero,'Granada')
     elif numero == "6":
-        print("Derecha + Disparar")
-        pyautogui.keyDown('a')
-        pyautogui.keyDown('right')
-        time.sleep(1)
-        pyautogui.keyUp('right')
-        pyautogui.keyUp('a')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonDoble('rigth', 'a', index, numero, 'Derecha + Disparo')
     elif numero == "7":
-        print("Arriba + Disparo")
-        pyautogui.keyDown('up')
-        pyautogui.keyDown('a')
-        time.sleep(1)
-        pyautogui.keyUp('up')
-        pyautogui.keyUp('a')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonDoble('up', 'a', index, numero, 'Arriba + Disparo')
     elif numero == "8":
-        print("Salto + Derecha")
-        pyautogui.keyDown('d')
-        pyautogui.keyDown('right')
-        time.sleep(1)
-        pyautogui.keyUp('right')
-        pyautogui.keyUp('d')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonDoble('d', 'right', index, numero, 'Salto + Derecho')
     elif numero == "9":
-        print("Salto + Izquierda")
-        pyautogui.keyDown('d')
-        pyautogui.keyDown('left')
-        time.sleep(1)
-        pyautogui.keyUp('left')
-        pyautogui.keyUp('d')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonDoble('d', 'left', index, numero, 'Salto + Izquierda')
