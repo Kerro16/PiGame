ca6a5030bc6ac422450284035e7751f4906d0a09
TheR20 <r20_banana@hotmail.com>
2023-12-02 17:39:28 -0600

    Refactorizacion de codigo en jueguitos para vampire

--- clases/juegos.py
+++ clases/juegos.py
@@ -24,69 +24,28 @@ def enterautomatico(index):
     pyautogui.keyUp('enter')
 
 
-
 #Vampire Survivors /arriba, abajo, izquierda, derecha y enter/
 def vampire(numero, index):
     if numero == "0":
-        print("Arriba")
-        pyautogui.keyDown('up')
-        time.sleep(1)
-        pyautogui.keyUp('up')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('up',index,numero,'Arriba')
     elif numero == "1":
-        print("Abajo")
-        pyautogui.keyDown('down')
-        time.sleep(1)
-        pyautogui.keyUp('down')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('down',index,numero,'Abajo')
     elif numero == "2":
-        print("Izquierda")
-        pyautogui.keyDown('left')
-        time.sleep(1)
-        pyautogui.keyUp('left')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('left',index,numero,'Izquierda')
     elif numero == "3":
-        print("Derecha")
-        pyautogui.keyDown('right')
-        time.sleep(1)
-        pyautogui.keyUp('right')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('right',index,numero,'Derecha')
     elif numero == "4":
-        print("Enter")
-        pyautogui.keyDown('enter')
-        time.sleep(1)
-        pyautogui.keyUp('enter')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('enter',index,numero,'Enter')
     elif numero == "5":
-        print("Izquierda")
-        pyautogui.keyDown('left')
-        time.sleep(1)
-        pyautogui.keyUp('left')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('left',index,numero,'Izquierda')
     elif numero == "6":
-        print("Derecha")
-        pyautogui.keyDown('right')
-        time.sleep(1)
-        pyautogui.keyUp('right')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('right',index,numero,'Derecha')
     elif numero == "7":
-        print("Arriba")
-        pyautogui.keyDown('up')
-        time.sleep(1)
-        pyautogui.keyDown('up')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('up',index,numero,'Arriba')
     elif numero == "8":
-        print("Abajo")
-        pyautogui.keyDown('down')
-        time.sleep(1)
-        pyautogui.keyUp('down')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('down',index,numero,'Abajo')
     elif numero == "9":
-        print("Enter")
-        pyautogui.keyDown('enter')
-        time.sleep(1)
-        pyautogui.keyUp('enter')
-        return "El numero de PI es {} y esta en la posicion {}".format(numero, index)
+        return presionarBotonSencillo('enter',index,numero,'Enter')
 
 
 # Metal Slug /arriba, abajo, izquierda, derecha, a , s , d  se agrega un enter cada que el index pasa por 9 iteraciones/
