77095337d9dc46955389301af9a65778eb157d7e
Kerro16 <kerrox16@gmail.com>
2023-12-06 16:10:13 -0600

    Clase lector refactorizada

--- clases/lector.py
+++ clases/lector.py
@@ -17,9 +17,8 @@ funciones_juegos = {
 def obtener_path():
     root = tk.Tk()
     root.withdraw()
-    file_path = "C:/Users/R20/Desktop/pi-billion.txt"  # filedialog.askopenfilename(title="Selecciona un archivo")
+    file_path = "C:/Users/fas_c/Desktop/pi-billion.txt"  # filedialog.askopenfilename(title="Selecciona un archivo")
     return file_path
-
 
 # Se lee el archivo donde tenemos pi
 def leerarchivo(file_path):
@@ -29,21 +28,28 @@ def leerarchivo(file_path):
 
             print("Archivo leido exitosamente")
             time.sleep(2)
-            # tomamos cada numero de pi y lo mandamos a juegos.py donde lo convierte a inputs de taclado solo es necesario cambiar el clase.juegos.xxx por el deseado
-            for index in range(len(archivotxxt)):
-                numeroPI = archivotxxt[index]
-                # verificamos la variable global que mandamos desde interfaz en la clase enviar_nombre y con ello indicaremos cual juego es el que queremos llamar desde la clase juegospy
-                funcion_juego = funciones_juegos.get(clases.globales.mi_variable_global)
-                print(funcion_juego(numeroPI, index))
 
     except FileNotFoundError:
         print("Archivo no encontrado, Por favor revisar la ruta")
     except Exception as e:
         print(f"Ocurrio un error: {e}")
 
+    return archivotxxt
+
+def recorrer_pi(archivotxxt):
+
+    # tomamos cada numero de pi y lo mandamos a juegos.py donde lo convierte a inputs de taclado solo es necesario cambiar el clase.juegos.xxx por el deseado
+    for index in range(len(archivotxxt)):
+        numeroPI = archivotxxt[index]
+        # verificamos la variable global que mandamos desde interfaz en la clase enviar_nombre y con ello indicaremos cual juego es el que queremos llamar desde la clase juegospy
+        funcion_juego = funciones_juegos.get(clases.globales.mi_variable_global)
+        print(funcion_juego(numeroPI, index))
 
 # Este se manda llamar para iniciar con la lectura de el archivo.
 def iniciarlector():
-    file_path = obtener_path()
-    if file_path:
-        leerarchivo(file_path)
+
+   file_path = obtener_path()
+   archivotxxt = leerarchivo(file_path)
+   if archivotxxt:
+     recorrer_pi(archivotxxt)
+
